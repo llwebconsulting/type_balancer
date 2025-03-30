1aabf3b015d97234354a6f21a1a753a2d8e1240f
Carl Smith <carl@llwebconsulting.com>
2025-03-29 21:47:11 -0400

    updated tests

--- spec/type_balancer/gap_fillers/alternating_spec.rb
+++ spec/type_balancer/gap_fillers/alternating_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe TypeBalancer::GapFillers::Alternating do
   subject(:filler) { described_class.new(collection, primary_items, secondary_items) }
 
   let(:collection) { Array.new(6) }
+  let(:primary_items) { %w[P1 P2 P3] }
+  let(:secondary_items) { %w[S1 S2 S3] }
 
   describe '#fill_gaps' do
     let(:positions) { (0...collection.size).to_a }
-    let(:primary_items) { %w[A1 A2 A3] }
-    let(:secondary_items) { %w[B1 B2 B3] }
 
     context 'with available items' do
       it 'alternates between primary and secondary items' do
         result = filler.fill_gaps(positions)
-        expect(result).to eq(%w[A1 B1 A2 B2 A3 B3])
+        expect(result).to eq(%w[P1 S1 P2 S2 P3 S3])
       end
 
       it 'handles when primary items run out' do
         filler = described_class.new(collection, [], secondary_items)
         result = filler.fill_gaps(positions)
-        expect(result).to eq(['B1', 'B2', 'B3', nil, nil, nil])
+        expect(result).to eq(['S1', 'S2', 'S3', nil, nil, nil])
       end
 
       it 'handles when secondary items run out' do
         filler = described_class.new(collection, primary_items, [])
         result = filler.fill_gaps(positions)
-        expect(result).to eq(['A1', 'A2', 'A3', nil, nil, nil])
+        expect(result).to eq(['P1', 'P2', 'P3', nil, nil, nil])
       end
     end
 
@@ -37,7 +37,23 @@ RSpec.describe TypeBalancer::GapFillers::Alternating do
 
       it 'fills only nil positions alternating between primary and secondary items' do
         result = filler.fill_gaps(positions)
-        expect(result).to eq(%w[X1 A1 X2 B1 X3 A2])
+        expect(result).to eq(%w[X1 P1 X2 S1 X3 P2])
+      end
+    end
+
+    context 'when positions array is empty' do
+      it 'returns the collection unchanged' do
+        expect(filler.fill_gaps([])).to eq(collection)
+      end
+    end
+
+    context 'when some positions are already filled' do
+      let(:collection) { ['X1', nil, 'X2', nil, 'X3', nil] }
+      let(:positions) { [0, 1, 2, 3, 4, 5] }
+
+      it 'only fills nil positions' do
+        result = filler.fill_gaps(positions)
+        expect(result).to eq(%w[X1 P1 X2 S1 X3 P2])
       end
     end
   end
